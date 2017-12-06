4a3afe9f5dac1cad1b9629ce14bba4aa6637af78
hugonardo <hugonardo@gmail.com>
2017-12-06 20:02:06 -0200

    [fix] ViewBindingAdapter.java NullPointerException
    Check null in visibility boolean parameter to avoid NPE. If visibility is null,
    them it is considered false.

--- library/src/main/java/net/hugonardo/android/commons/databinding/ViewBindingAdapter.java
+++ library/src/main/java/net/hugonardo/android/commons/databinding/ViewBindingAdapter.java
@@ -2,14 +2,16 @@ package net.hugonardo.android.commons.databinding;
 
 import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
+import static net.hugonardo.java.commons.Booleans.isTrue;
 
 import android.databinding.BindingAdapter;
 import android.view.View;
+import javax.annotation.Nullable;
 
 public class ViewBindingAdapter {
 
     @BindingAdapter({"android:visibility"})
-    public static void setVisible(View view, Boolean visible) {
-        view.setVisibility(visible ? VISIBLE : GONE);
+    public static void setVisible(View view, @Nullable Boolean visible) {
+        view.setVisibility(isTrue(visible) ? VISIBLE : GONE);
     }
 }
